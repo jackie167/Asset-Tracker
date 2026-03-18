4db9e46ba350484691fe12da8c4a99931c4a6e43
Dinh Thi <thi.ledinh@gmail.com>
2026-03-18 10:52:57 +0700

    Add root index.js fallback for Railpack

---
+++ index.js
@@ -0,0 +1,11 @@
+import { spawn } from "node:child_process";
+
+const child = spawn("pnpm", ["-C", "api-server", "start"], { stdio: "inherit" });
+
+child.on("exit", (code, signal) => {
+  if (signal) {
+    process.kill(process.pid, signal);
+    return;
+  }
+  process.exit(code ?? 1);
+});
